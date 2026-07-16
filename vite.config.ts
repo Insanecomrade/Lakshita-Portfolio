cee942edb54a3d1381c6b286a01b49bb1b8684e7
Dev.S <143781122+Insanecomrade@users.noreply.github.com>
2026-07-16 19:26:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Lakshita-Portfolio/", // Must match your repository name exactly
-
   server: {
     host: "::",
     port: 8080,
   },
-
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
